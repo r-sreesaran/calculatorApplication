4fdb1b97f428137c0d41cf35bb0e191b25b7e694
r-sreesaran <r.sreesaran@gmail.com>
2021-08-23 22:00:43 +0530

    added tests

---
+++ src/test/java/org/sree/services/test/TestClass.java
@@ -0,0 +1,33 @@
+package org.sree.services.test;
+
+import org.sree.services.Calculator;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class TestClass {
+    int i =0;
+    @Test(priority = 1)
+    public void Method1() {
+
+        Reporter.log("value of i is "+i);
+        Assert.assertTrue(i ==1,"value of i is "+i);
+    }
+
+    @Test(priority = 2)
+    public void Method2() {
+
+        Reporter.log("value of i is "+i);
+        Assert.assertTrue(i ==2,"value of i is "+i);
+
+    }
+    @Test(priority = 3)
+    public void Method3() {
+
+        Reporter.log("value of i is "+i);
+        Assert.assertTrue(i ==3,"value of i is "+i);
+
+    }
+}
---
+++ src/test/java/org/sree/services/test/Testclass2.java
@@ -0,0 +1,30 @@
+package org.sree.services.test;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testclass2 {
+    int i =0;
+    @Test(priority = 1)
+    public void Method1() {
+
+        Reporter.log("value of i is "+i);
+        Assert.assertTrue(i ==1,"value of i is "+i);
+    }
+
+    @Test(priority = 2)
+    public void Method2() {
+
+        Reporter.log("value of i is "+i);
+        Assert.assertTrue(i ==2,"value of i is "+i);
+
+    }
+    @Test(priority = 3)
+    public void Method3() {
+
+        Reporter.log("value of i is "+i);
+        Assert.assertTrue(i ==3,"value of i is "+i);
+
+    }
+}
