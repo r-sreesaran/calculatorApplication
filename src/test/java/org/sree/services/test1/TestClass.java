edb0f0281832d1acae2a6e4e00953fac993a8718
r-sreesaran <r.sreesaran@gmail.com>
2021-08-27 22:05:51 +0530

    test failures adaption

--- src/test/java/org/sree/services/test1/TestClass.java
+++ src/test/java/org/sree/services/test1/TestClass.java
@@ -8,21 +8,21 @@ public class TestClass {
     int i =0;
     @Test(priority = 1)
     public void Method1() {
-
+        i++;
         Reporter.log("value of i is "+i);
         Assert.assertTrue(i ==1,"value of i is "+i);
     }
 
     @Test(priority = 2)
     public void Method2() {
-
+        i++;
         Reporter.log("value of i is "+i);
         Assert.assertTrue(i ==2,"value of i is "+i);
 
     }
     @Test(priority = 3)
     public void Method3() {
-
+        i++;
         Reporter.log("value of i is "+i);
         Assert.assertTrue(i ==3,"value of i is "+i);
 
